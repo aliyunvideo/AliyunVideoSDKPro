bf7055c25926e25faf779d78428b3230e0dfadfd
蔡泽坚 <wb-czj380013@alibaba-inc.com>
2018-08-27 15:06:42 +0800

    release

--- frameworks/AliyunVideoSDKPro.framework/Headers/AliyunVideoSDKInfo.h
+++ frameworks/AliyunVideoSDKPro.framework/Headers/AliyunVideoSDKInfo.h
@@ -4,7 +4,11 @@
 //
 //  Created by Worthy on 2017/5/23.
 //  Copyright © 2017年 Alibaba Group Holding Limited. All rights reserved.
-//
+// BUILD INFO
+// AliyunAlivcCommitId:61c74b281
+// AliyunMediaCoreCommitId:3ef3e847
+// AliyunVideoSDKCommitId:f7f4fd71
+// AliyunVideoSDKBuildId:3453961
 
 #import <Foundation/Foundation.h>
 
@@ -12,4 +16,13 @@
 
 + (NSString *)version;
 
++ (NSString *)alivcCommitId;
+
++ (NSString *)mediaCoreCommitId;
+
++ (NSString *)videoSDKCommitId;
+
++ (NSString *)videoSDKBuildId;
+
++ (void)printSDKInfo;
 @end
